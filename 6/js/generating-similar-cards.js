17f15405600554d1b212f9d6d9d7b394895ce71d
Keks <keks@htmlacademy.ru>
2022-11-11 06:07:15 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/generating-similar-cards.js
+++ 6/js/generating-similar-cards.js
@@ -19,22 +19,11 @@ similarOfferCard.forEach((offerCard) => {
   offerCardElement.querySelector('.popup__type').textContent = getRusTypeOfCard(offerCard.offer.type);
   offerCardElement.querySelector('.popup__text--capacity').textContent = `${getRooms(offerCard.offer.rooms)} для ${offerCard.offer.guests} гостей`;
   offerCardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offerCard.offer.checkin}, выезд после ${offerCard.offer.checkout}`;
-  offerCardElement.querySelector('.popup__features').textContent = offerCard.offer.features;
+  offerCardElement.querySelector('.popup__features').textContent = `Присутствуют сдедующие удобства: ${offerCard.offer.features}`;
   offerCardElement.querySelector('.popup__description').textContent = offerCard.offer.description;
   const arrayWithPhotos = offerCard.offer.photos;
   getSeparatePhoto(arrayWithPhotos, clonedPhoto, nodeOfPhotos);
 
-  // const elementForClone
-  // пропустил пунк с фото, надо проконсультироваться, я заебался искать инфу 4 часа, возможно, опять ебаный затуп
-
   offerCardElement.querySelector('.popup__avatar').src = offerCard.author.avatar;
   mapCanvas.append(offerCardElement);
 });
-
-
-/* В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как атрибут src соответствующего изображения.
-Список offer.photos - это массив с ссылками. Получается, что с учётом того, что может прийти разное кол-во ссылок из массива offer.photos, надо создать кол-во изображений исходя из длины массива offer.photos. Получается, что нужно написать функцию, которая будет клонировать узел исходя из длины массива + записывать в этот узел значение src, которое будет лежать в массиве offer.photos через индекс
-
-Функция будет принимать массив offer.photos и узел, который надо клонировать. В функции мы будем клонировать узел и добавлять его как потомка к
-
-*/
